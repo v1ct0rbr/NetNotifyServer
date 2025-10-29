2e1658156daeeec1367ff4649b2bb2b57bf7b393
queirogavictor-at-892414404840 <victorqueiroga.jp@gmail.com>
2025-10-29 15:28:36 -0300

    feat: Add AuthController for authentication API endpoints

---
+++ src/main/java/br/gov/pb/der/netnotify/controller/AuthController.java
@@ -0,0 +1,15 @@
+package br.gov.pb.der.netnotify.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+public class AuthController {
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+   
+
+}
